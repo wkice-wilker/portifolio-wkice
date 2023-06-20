c27f06149094b60f3014b5fe0464dec6328391d8
wkice-wilker <92658448+wkice-wilker@users.noreply.github.com>
2023-06-20 16:20:31 -0300

    correção na lógica do javascript navbar
    Signed-off-by: wkice-wilker <92658448+wkice-wilker@users.noreply.github.com>

--- js/wkice/navbar.js
+++ js/wkice/navbar.js
@@ -1,6 +1,7 @@
-
+ 
     var links = document.querySelectorAll('.navbar a');
     var linksTop = document.querySelectorAll('.footer-iconTop a');
+    var linksHeader = document.querySelectorAll('.logo');
     var sections = document.querySelectorAll('section');
 
 links.forEach(function(link) {
@@ -28,6 +29,7 @@ links.forEach(function(link) {
     });
 });
 
+
 // voltar a cor para o home quando clicar no icone de ir para o início da página
 linksTop.forEach(function(linkTop) {
     linkTop.addEventListener('click', function() {
@@ -40,5 +42,34 @@ linksTop.forEach(function(linkTop) {
         links.forEach(function(link) {
             link.style.color = ''; // Utiliza a cor padrão (herdada)
         });
+    });
+});
+
+
+// voltar a cor para o home quando clicar no icone Wkice
+linksHeader.forEach(function(link) {
+    link.addEventListener('click', function() {
+        var selectedSectionsId = this.getAttribute('href').substring(1); // Obtém o ID da seção correspondente ao link clicado
+        if (selectedSectionsId !== '#home') {
+            var homeLink = document.querySelector('.navbar a[href="#home"]');
+            homeLink.classList.add('ativado');
+
+        }else if(homeLink1.classList('ativado')){
+            var homeLink1 = document.querySelector('.header a[href="#home"]');
+            homeLink1.classList.remove('ativado');
+        }
+        // Resetar a cor de todos os links        
+        links.forEach(function(link) {
+            link.style.color = ''; // Utiliza a cor padrão (herdada)
+        });
+        // Alterar a cor do link clicado
+         this.classList.add('ativado');
+    });
+    links.forEach(function(link) {
+        link.addEventListener('click', function() {
+            links.forEach(function(link) {
+                link.classList.remove('ativado');
+             });
+         });
     });
 });
