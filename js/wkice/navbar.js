bc2ef643d9105166c5d0fd51c3b12c4e754a5060
wkice-wilker <92658448+wkice-wilker@users.noreply.github.com>
2023-12-13 12:17:24 -0300

    responsividade

--- js/wkice/navbar.js
+++ js/wkice/navbar.js
@@ -1,75 +1,36 @@
- 
-    var links = document.querySelectorAll('.navbar a');
-    var linksTop = document.querySelectorAll('.footer-iconTop a');
-    var linksHeader = document.querySelectorAll('.logo');
-    var sections = document.querySelectorAll('section');
 
-links.forEach(function(link) {
-    link.addEventListener('click', function() {
-        var selectedSectionsId = this.getAttribute('href').substring(1); // Obtém o ID da seção correspondente ao link clicado
-        if (selectedSectionsId !== '.navbar a') {
-            var homeLink = document.querySelector('a[href="#home"]');
-            homeLink.classList.add('ativado');
-          }
-        // Resetar a cor de todos os links        
-        links.forEach(function(link) {
-            link.style.color = ''; // Utiliza a cor padrão (herdada)
-        });
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
 
-        // Alterar a cor do link clicado
-         this.style.color = 'var(--main-color)';
-    });
+menuIcon.onclick = ()=> {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('ativado');
+};
 
-    links.forEach(function(link) {
-        link.addEventListener('click', function() {
-            links.forEach(function(link) {
-                link.classList.remove('ativado');
-             });
-         });
-    });
-});
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-// voltar a cor para o home quando clicar no icone de ir para o início da página
-linksTop.forEach(function(linkTop) {
-    linkTop.addEventListener('click', function() {
-        var selectedSectionsId = this.getAttribute('href'); // Obtém o ID da seção correspondente ao link clicado
-        if (selectedSectionsId !== '.footer-iconTop a') {
-            var homeLink = document.querySelector('.navbar a[href="#home"]');//seleciona o link home da navbar para adicionar a classe 'ativado'
-            homeLink.classList.add('ativado');
-          }
-        // Resetar a cor de todos os links        
-        links.forEach(function(link) {
-            link.style.color = ''; // Utiliza a cor padrão (herdada)
-        });
-    });
-});
-
-
-// voltar a cor para o home quando clicar no icone Wkice
-linksHeader.forEach(function(link) {
-    link.addEventListener('click', function() {
-        var selectedSectionsId = this.getAttribute('href').substring(1); // Obtém o ID da seção correspondente ao link clicado
-        if (selectedSectionsId !== '#home') {
-            var homeLink = document.querySelector('.navbar a[href="#home"]');
-            homeLink.classList.add('ativado');
-
-        }else if(homeLink1.classList('ativado')){
-            var homeLink1 = document.querySelector('.header a[href="#home"]');
-            homeLink1.classList.remove('ativado');
+        if(top >= offset && top < offset + height) {
+            // adiciona e remove a classe ativado de acordo com o id do navbar
+            navLinks.forEach(links => {
+                links.classList.remove('ativado');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('ativado');
+            })
         }
-        // Resetar a cor de todos os links        
-        links.forEach(function(link) {
-            link.style.color = ''; // Utiliza a cor padrão (herdada)
-        });
-        // Alterar a cor do link clicado
-         this.classList.add('ativado');
-    });
-    links.forEach(function(link) {
-        link.addEventListener('click', function() {
-            links.forEach(function(link) {
-                link.classList.remove('ativado');
-             });
-         });
-    });
-});
+    })
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky' , window.scrollY > 100);
+
+    // remover o menu em tamanho de celular quando selecionar uma pagina
+    menuIcon.classList.remove('bx-x');
+    navBar.classList.remove('ativado');
+}
